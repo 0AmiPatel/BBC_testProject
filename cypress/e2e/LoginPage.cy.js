3f3bbed2deffb4f078dc85eb2af165e0db7c754f
AmiPiyush <amipiyush@gmail.com>
2023-10-20 11:11:30 +0100

    Initial Commit

---
+++ cypress/e2e/LoginPage.cy.js
@@ -0,0 +1,20 @@
+import Login from "../PageObject/LoginPage.js"
+
+    describe('Calculator Verification ', () => {
+       
+            it('HomepageTests', () => {
+                cy.visit('https://www.calculator.net/')
+
+                const ln = new Login()
+                ln.chkLogoVisibility();
+                ln.chkCalcVisibility();
+                ln.AllCaculatorBtnClick();
+              })
+      
+                 
+    
+      })
+
+
+
+   
